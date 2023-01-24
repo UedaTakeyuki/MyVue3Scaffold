71accb9a000742282c02ce8a22631fd9004792a5
Takeyuki UEDA <ueda@latelierdueda.com>
2023-01-24 10:36:40 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import analyze from "rollup-plugin-analyzer"
 import { visualizer } from 'rollup-plugin-visualizer'
-import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,9 +14,6 @@ export default defineConfig({
       plugins: [
         analyze(), 
         visualizer(),
-        viteExternalsPlugin({
-          vue: 'Vue',
-        }),
       ]
     },
   },
